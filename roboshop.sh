2f509c7e1acc705e40c3056313874f3c6663b63b
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-01-30 12:35:41 +0530

    roboshop automation with shell

--- roboshop.sh
+++ roboshop.sh
@@ -34,13 +34,13 @@ do
     if [ $instance == "frontend" ]; then
         IP=$( aws ec2 describe-instances \
         --instance-ids $INSTANCE_ID \
-        --query "Reservations[].Instances[].PublicIpAddress" )
+        --query "Reservations[].Instances[].PublicIpAddress" --output text )
 
         RECORD_NAME="$DOMAIN_NAME"
     else
         IP=$( aws ec2 describe-instances \
         --instance-ids $INSTANCE_ID \
-        --query "Reservations[].Instances[].PrivateIpAddress" )
+        --query "Reservations[].Instances[].PrivateIpAddress" --output text )
 
         RECORD_NAME="$instance.$DOMAIN_NAME"
     fi
